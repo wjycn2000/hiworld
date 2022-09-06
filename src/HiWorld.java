3edc2a6877accc6d505aa7cf22e562c9e08f937d
wjycn2000 <112606260+wjycn2000@users.noreply.github.com>
2022-09-06 08:53:21 +0800

    Update HiWorld.java

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,5 +1,6 @@
 public class HiWorld {
     public static void main(String[] args) {
+        //print greetings
         System.out.println("Hi World");
         System.out.println("Hi World");
     }
